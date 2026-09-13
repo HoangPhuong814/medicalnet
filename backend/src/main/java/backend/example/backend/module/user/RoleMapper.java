ef2c9861133d6aa5003a67cc58e53ff98f60e510
Hoang Phuong <155792523+DiazMarco118@users.noreply.github.com>
2026-09-13 22:00:44 +0700

    Refactor RoleMapper

--- backend/src/main/java/backend/example/backend/module/user/RoleMapper.java
+++ backend/src/main/java/backend/example/backend/module/user/RoleMapper.java
@@ -3,12 +3,22 @@ package backend.example.backend.module.user;
 import backend.example.backend.module.user.dto.RoleRequest;
 import backend.example.backend.module.user.dto.RoleResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "permissions", ignore = true)
     Role toRole(RoleRequest request);
+
+    @Mapping(target = "permissions", source = "permissions")
     RoleResponse toRoleResponse(Role role);
+
+    default String mapPermissionToString(Permission permission) {
+        return permission != null ? permission.getName() : null;
+    }
+
     List<RoleResponse> toListRoleResponse(List<Role> roles);
 }
